c1ac29e12aa0654a32f8358597d5473f6cb391d3
jaba <jaba@geometry.ooo>
2024-10-06 14:26:14 +0300

    Initial commit

---
+++ src/main/java/is/fistlab/FistLabApplication.java
@@ -0,0 +1,13 @@
+package is.fistlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FistLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/fistlab/FistLabApplicationTests.java
@@ -0,0 +1,13 @@
+package is.fistlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FistLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
