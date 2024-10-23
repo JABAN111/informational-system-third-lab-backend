582ec5f744b1b669c3900cdf60f8e68b76e887fc
jaba <jaba@geometry.ooo>
2024-10-24 00:44:24 +0300

    временно(или безвременно) убраны тесты

--- src/test/java/is/fistlab/controllers/StudyGroupControllerTest.java
+++ src/test/java/is/fistlab/controllers/StudyGroupControllerTest.java
@@ -1,19 +1,19 @@
-//package is.fistlab.controllers;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//class StudyGroupControllerTest {
-//
-//    @BeforeEach
-//    void setUp() {
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//    }
-//}
+package is.fistlab.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudyGroupControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
