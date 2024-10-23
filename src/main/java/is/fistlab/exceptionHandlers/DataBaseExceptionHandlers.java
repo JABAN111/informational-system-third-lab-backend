d7a35d2f657be838580e170038f1616c14109dcf
jaba <jaba@geometry.ooo>
2024-10-24 00:36:37 +0300

    добавлен хэндлер для ошибок базы данных

--- src/main/java/is/fistlab/exceptionHandlers/DataBaseExceptionHandlers.java
+++ src/main/java/is/fistlab/exceptionHandlers/DataBaseExceptionHandlers.java
@@ -1,8 +1,18 @@
 package is.fistlab.exceptionHandlers;
 
+import is.fistlab.mappers.exceptions.InvalidFieldException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class DataBaseExceptionHandlers {
+
+    @ExceptionHandler(InvalidFieldException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String fieldInvalid(InvalidFieldException exc){
+        return exc.getMessage();
+    }
 
 }
