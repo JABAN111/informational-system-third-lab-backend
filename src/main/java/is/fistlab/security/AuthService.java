3f66ecff530f7ce5fc551bae1397f35562ca9c81
jaba <jabannishe@gmail.com>
2024-11-07 21:51:03 +0300

    Добавлен AuthService

---
+++ src/main/java/is/fistlab/security/AuthService.java
@@ -0,0 +1,50 @@
+package is.fistlab.security;
+
+import is.fistlab.database.entities.User;
+import is.fistlab.database.enums.UserRole;
+import is.fistlab.dto.JwtAuthenticationResponse;
+import is.fistlab.dto.UserDto;
+import is.fistlab.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationResponse signUp(UserDto requestDto) {
+
+        var user = User.builder()
+                .username(requestDto.getUsername())
+                .password(passwordEncoder.encode(requestDto.getPassword()))
+                .role(UserRole.valueOf(requestDto.getRole()))
+                .build();
+
+        userService.createNewUser(user);
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+
+    public JwtAuthenticationResponse signIn(UserDto request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getUsername(),
+                request.getPassword()
+        ));
+
+        var user = userService
+                .getUserDetailsService()
+                .loadUserByUsername(request.getUsername());
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+}
