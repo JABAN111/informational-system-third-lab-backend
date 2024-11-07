4cf06772506493174daafa0c3b0857a3056e960b
jaba <jabannishe@gmail.com>
2024-11-07 21:35:28 +0300

    User теперь является реализацией UserDetails(необходимо для работы spring security)

--- src/main/java/is/fistlab/database/entities/User.java
+++ src/main/java/is/fistlab/database/entities/User.java
@@ -2,16 +2,22 @@ package is.fistlab.database.entities;
 
 import is.fistlab.database.enums.UserRole;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
 @Entity
 @NoArgsConstructor
 @ToString
 @Table(name = "app_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +32,28 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
