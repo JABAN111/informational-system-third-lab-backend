ee32634f5a82eb9ac2f459f5e297f6eb269b9b42
jaba <jabannishe@gmail.com>
2024-12-18 04:42:06 +0300

    Изменена конфигурация, удалены лишние классы

--- src/main/java/is/fistlab/configurations/AsyncConfiguration.java
+++ src/main/java/is/fistlab/configurations/AsyncConfiguration.java
@@ -1,24 +1,23 @@
 package is.fistlab.configurations;
 
 import is.fistlab.RejectedExecutionHandlerImpl;
+import is.fistlab.exceptions.ImportAsyncUncaughtExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 
 @EnableAsync
 @Configuration
 @Slf4j
-public class AsyncConfiguration {
+public class AsyncConfiguration implements AsyncConfigurer{
 
-//    @Override
+    @Override
     @Bean
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
@@ -26,31 +25,14 @@ public class AsyncConfiguration {
         executor.setMaxPoolSize(20);
         executor.setQueueCapacity(50);
         executor.setThreadNamePrefix("AsyncTaskThread::");
-
-//        executor.setQueueCapacity();
         executor.setRejectedExecutionHandler(new RejectedExecutionHandlerImpl());
-
         executor.setWaitForTasksToCompleteOnShutdown(true);
-        log.info("start init");
-//        if(true)
-//            throw new RuntimeException("executor requested: " + executor);
         executor.initialize();
         return executor;
     }
 
-
-
-//    @Override
+    @Bean
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-
-        return new AsyncUncaughtExceptionHandler() {
-            @Override
-            public void handleUncaughtException(Throwable ex,
-                                                Method method, Object... params) {
-                log.error(ex.getMessage(), ex);
-                ex.printStackTrace();
-            }
-        };
-
+        return new ImportAsyncUncaughtExceptionHandler();
     }
 }
--- src/main/java/is/fistlab/controllers/ImportController.java
+++ src/main/java/is/fistlab/controllers/ImportController.java
@@ -1,16 +1,19 @@
 package is.fistlab.controllers;
 
+import is.fistlab.SequentialQueueProcessor;
+import is.fistlab.database.entities.User;
 import is.fistlab.database.repositories.LocationRepository;
 import is.fistlab.database.repositories.PersonRepository;
 import is.fistlab.database.repositories.StudyGroupRepository;
-import is.fistlab.services.AsyncService;
-import is.fistlab.services.impl.AsyncServiceImpl;
+import is.fistlab.services.AuthService;
+import is.fistlab.services.ImportService;
+import is.fistlab.services.PersonService;
+import is.fistlab.services.StudyGroupService;
 import is.fistlab.services.impl.AuthServiceImpl;
-import is.fistlab.services.impl.ssyncProcessing.AsyncProcessing;
-import is.fistlab.utils.AuthenticationUtils;
+import is.fistlab.services.impl.ssyncProcessing.ImportProcessingImpl;
+import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,43 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+
 
 @RestController
 @RequestMapping("/api/v1/import")
 @AllArgsConstructor
 @Slf4j
 public class ImportController {
-    private final AsyncService importService;
-    private final StudyGroupRepository studyGroupRepository;
-    private final LocationRepository locationRepository;
-    private final PersonRepository personRepository;
-    private final AsyncProcessing asyncProcessing;
-    private final AuthServiceImpl authServiceImpl;
-    private final AsyncServiceImpl asyncServiceImpl;
+    private final ImportService importService;
+    private final AuthService authService;
 
     @PostMapping("/csv")
-    public ResponseEntity<Response<Integer>> importStudyGroups(@RequestParam("file") MultipartFile file){
-        try {
-            String fileName = file.getOriginalFilename();
-            var fil = asyncServiceImpl.readFile(file.getInputStream());
-//            var result = importService.importStudyGroups(file.getInputStream());
-            asyncProcessing.processAsync(fil, authServiceImpl.getCurrentUser());
-            return ResponseEntity.ok(new Response<>("Сохранение начали"));
-//                            result)
-        }catch (Exception e){
-            log.error(e.getMessage());
-            throw new NotImplementedException("csv");
+    public ResponseEntity<Response<Integer>> importStudyGroups(@RequestParam("file") final MultipartFile file, @RequestParam("userTimestamp") final Timestamp userTimestamp) throws IOException {
+        var currentUser = authService.getCurrentUser();
 
-        }
+        importService.importFile(file, currentUser, userTimestamp);
+        return ResponseEntity.ok(new Response<>("Сохранение начали"));
     }
-
 
     @PostMapping("/drop")
-    public void dropAll(){
-        studyGroupRepository.deleteAll();
-        personRepository.deleteAll();
-        locationRepository.deleteAll();
+    public void dropAll() {
+        importService.dropAll();
     }
+
 }
 
 
