16bbe28f980393445c6478db9a5cda1b159c0edb
jaba <368601@edu.itmo.ru>
2025-02-05 03:44:40 +0300

    Added transaction to SequentialQueueProcessor

--- src/main/java/is/fistlab/SequentialQueueProcessor.java
+++ src/main/java/is/fistlab/SequentialQueueProcessor.java
@@ -10,29 +10,38 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionTemplate;
+
 @Component
 @Slf4j
 public class SequentialQueueProcessor {
     private final BlockingQueue<Runnable> taskQueue = new LinkedBlockingQueue<>();
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final PlatformTransactionManager transactionManager;
 
-    public SequentialQueueProcessor() {
+    public SequentialQueueProcessor(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+
         executorService.submit(() -> {
             while (true) {
                 try {
                     log.info("current queue size: {}", taskQueue.size());
                     Runnable task = taskQueue.take();
                     try {
-                        task.run();
+                        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+                        transactionTemplate.execute((TransactionStatus status) -> {
+                            task.run();
+                            return null;
+                        });
                     } catch (Exception taskException) {
                         if(taskException instanceof InvalidFieldException) {
                             log.error("Invalid field: {}", taskException.getMessage());
                         }
-
                         log.error("Error occurred during task execution: ", taskException);
                     }
                     log.info("current after executing size: {}", taskQueue.size());
-
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                     break;
